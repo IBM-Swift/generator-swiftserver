a7c17c1cbe80ff3ae8fd514cfe44b1855cd5cce2
Mike Tunnicliffe <mike.tunnicliffe@uk.ibm.com>
2017-02-18 02:23:26 +0000

    Fix CloudConfiguration minor to avoid version clash

--- refresh/templates/basicproject/Package.swift
+++ refresh/templates/basicproject/Package.swift
@@ -8,36 +8,36 @@ let package = Package(
     dependencies: [
 
 <% if(bluemix) { -%>
-        .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git",                                      majorVersion: 0),
+        .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git",     majorVersion: 0, minor: 0),
 <% } else { -%>
-        .Package(url: "https://github.com/IBM-Swift/Configuration.git",                                           majorVersion: 0),
+        .Package(url: "https://github.com/IBM-Swift/Configuration.git",          majorVersion: 0),
 <% } -%>
 <% Object.keys(services).forEach(function(serviceType) { -%>
 <% if(serviceType === 'cloudant') { -%>
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git",                                          majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git",         majorVersion: 1),
 <% } -%>
 <% if(serviceType === 'mongodb') { -%>
-        .Package(url: "https://github.com/tfrank64/MongoKitten.git",                                              majorVersion: 3),
+        .Package(url: "https://github.com/tfrank64/MongoKitten.git",             majorVersion: 3),
 <% } -%>
 <% if(serviceType === 'redis') { -%>
-        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git",                                            majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git",           majorVersion: 1),
 <% } -%>
 <% if(serviceType === 'postgresql') { -%>
-        .Package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL.git",                                  majorVersion: 0),
+        .Package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL.git", majorVersion: 0),
 <% } -%>
 <% if(serviceType === 'mysql') { -%>
-        .Package(url: "https://github.com/vapor/mysql",                                                           majorVersion: 1),
+        .Package(url: "https://github.com/vapor/mysql",                          majorVersion: 1),
 <% } -%>
 <% if(serviceType === 'objectstorage') { -%>
-        .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-objectstorage-serversdk-swift.git", majorVersion: 0),
+        .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix    -objectstorage-serversdk-swift.git", majorVersion: 0),
 <% } -%>
 <% }); -%>
 <% if (metrics)  { %>
-        .Package(url: "https://github.com/RuntimeTools/SwiftMetrics.git",                                         majorVersion: 0),
+        .Package(url: "https://github.com/RuntimeTools/SwiftMetrics.git",        majorVersion: 0),
 <% } -%>
 
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git",                                                  majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",                                            majorVersion: 1)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git",                 majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",           majorVersion: 1)
     ],
     exclude: []
 )
