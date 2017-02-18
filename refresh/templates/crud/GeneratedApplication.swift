f5ee6d4f6ed9d088aba12db96c70b67d017ae018
Mike Tunnicliffe <mike.tunnicliffe@uk.ibm.com>
2017-02-18 01:20:05 +0000

    Fix order of config loading

--- refresh/templates/crud/GeneratedApplication.swift
+++ refresh/templates/crud/GeneratedApplication.swift
@@ -10,8 +10,8 @@ public class GeneratedApplication {
     public init(configURL: URL) throws {
         router = Router()
         manager = try ConfigurationManager()
-                          .load(.environmentVariables)
                           .load(url: configURL)
+                          .load(.environmentVariables)
         factory = AdapterFactory(manager: manager)
 
         <%_ models.forEach(function(model) { _%>
