f3caed829d5fabaaac8c64d84ccb99501d847395
nhardman <nhardman@uk.ibm.com>
2017-03-06 15:22:28 +0000

    Add templated exe name to cli-config.yml (#104)

--- refresh/index.js
+++ refresh/index.js
@@ -843,9 +843,12 @@ module.exports = generators.Base.extend({
         this.templatePath('docker', 'Dockerfile'),
         this.destinationPath('Dockerfile')
       );
-      this.fs.copy(
+      this.fs.copyTpl(
         this.templatePath('docker', 'cli-config.yml'),
-        this.destinationPath('cli-config.yml')
+        this.destinationPath('cli-config.yml'),
+        {
+          executableName: this.executableModule,
+        }
       );
     },
 
