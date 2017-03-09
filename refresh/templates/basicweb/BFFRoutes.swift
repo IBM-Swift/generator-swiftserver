18655f3a6e8907a26e742b97d97fb20582020117
Robert F. Dickerson <rfdickerson@us.ibm.com>
2017-03-09 15:36:07 -0600

    added BFF route

--- refresh/templates/basicweb/BFFRoutes.swift
+++ refresh/templates/basicweb/BFFRoutes.swift
@@ -1,3 +1,27 @@
 func initializeBFFRoutes() {
 
+    router.get('/', function (req, res) {
+        request, response, next in 
+        response.send('Hello World!')
+        next()
+    })
+
+    router.post('/', function (req, res) { 
+        request, response, next in 
+        response.send('POST request at /')
+        next()
+    })
+
+    router.put('/', function (req, res) { 
+        request, response, next in 
+        response.send('PUT request at /')
+        next()
+    })
+
+    router.delete('/') { 
+        request, response, next in 
+        response.send('DELETE request at /')
+        next()
+    })
+
 }
