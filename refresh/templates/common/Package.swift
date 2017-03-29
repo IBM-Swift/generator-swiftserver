4b4edd824ab4f83e32f47b64b7aa40274741afce
Mike Tunnicliffe <mike.tunnicliffe@uk.ibm.com>
2017-03-29 11:44:07 +0000

    Tighten Kitura related dependencies to 1.6.x

--- refresh/templates/common/Package.swift
+++ refresh/templates/common/Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         <%- include(`../capabilities/${capabilityType}/importDependency.swift`) %>
 <%   } -%>
 <% }); -%>
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git",                 majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",           majorVersion: 1)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git",                 majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",           majorVersion: 1, minor: 6)
     ],
     exclude: ["src"]
 )
--- refresh/templates/services/cloudant/importDependency.swift
+++ refresh/templates/services/cloudant/importDependency.swift
@@ -1,1 +1,1 @@
-.Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git",  majorVersion: 1),
+.Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git",  majorVersion: 1, minor: 6),
