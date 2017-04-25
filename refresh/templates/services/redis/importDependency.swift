e90260e1b421fae61507fe91f9ee8691f9d0ddbf
Mike Tunnicliffe <mike.tunnicliffe@uk.ibm.com>
2017-04-25 12:04:22 +0000

    Update to Swift 3.1.1 and Kitura 1.7

--- refresh/templates/services/redis/importDependency.swift
+++ refresh/templates/services/redis/importDependency.swift
@@ -1,1 +1,1 @@
-        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1, minor: 7),
