bb54a9fa0c7b479f46cf52091e2e25c2ddc43e45
Robert Deans <robert.deans@uk.ibm.com>
2017-04-28 14:16:35 +0100

    Small format

--- refresh/templates/extensions/Manager.swift
+++ refresh/templates/extensions/Manager.swift
@@ -5,7 +5,7 @@ let defaultPort = 8080
 extension ConfigurationManager {
 
   public var port: Int { return (manager["PORT"] as? String).flatMap({ Int($0) }) ??
-                                 manager["port"] as? Int ??
-                                 defaultPort }
+                                manager["port"] as? Int ??
+                                defaultPort }
 
 }
