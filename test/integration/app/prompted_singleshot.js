2c8c5cdd97330d2dfbd6a488cee6ab6fff9a4851
Taylor Franklin <tfranklin@us.ibm.com>
2017-05-01 17:10:12 -0500

    added license

--- test/integration/app/prompted_singleshot.js
+++ test/integration/app/prompted_singleshot.js
@@ -1,3 +1,23 @@
+/*
+ * Copyright IBM Corporation 2016-2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Tests here do not stub out the subgenerators, so for the app generator
+ * the real build and refresh subgenerators get called.
+ */
 'use strict';
 var path = require('path');
 var assert = require('yeoman-assert');
