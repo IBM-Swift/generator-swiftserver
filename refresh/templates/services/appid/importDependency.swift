794a738945735b2a22ca2528bcc4e13dfd6fade4
Mike Tunnicliffe <mike.tunnicliffe@uk.ibm.com>
2017-05-05 17:04:06 +0000

    Narrow dependency on Kitura-Credentials

--- refresh/templates/services/appid/importDependency.swift
+++ refresh/templates/services/appid/importDependency.swift
@@ -1,2 +1,3 @@
         .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 1, minor: 6),
         .Package(url: "https://github.com/ibm-cloud-security/appid-serversdk-swift.git", majorVersion: 1),
