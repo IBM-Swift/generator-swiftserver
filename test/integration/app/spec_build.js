a19133d0c48cd4b2a824fcc24eb795a507b6b822
jkingoliver <king@us.ibm.com>
2017-05-15 12:24:58 -0500

    update test after moving init to extension file

--- test/integration/app/spec_build.js
+++ test/integration/app/spec_build.js
@@ -27,7 +27,7 @@ var rimraf = require('rimraf');
 var appGeneratorPath = path.join(__dirname, '../../../app');
 var buildGeneratorPath = path.join(__dirname, '../../../build');
 
-describe('Spec option and build integration tests for app generator', function () { 
+describe('Spec option and build integration tests for app generator', function () {
 
   describe('A CRUD application with a cloudant service is able to build', function () {
     // Swift build is slow so we need to set a longer timeout for the test
