b02f6753ea9efd01db9c261aaeadfe5cbb63e08a
jkingoliver <king@us.ibm.com>
2017-05-23 15:06:25 -0500

    change dependency repo back to public version

--- refresh/templates/services/pushnotifications/importDependency.swift
+++ refresh/templates/services/pushnotifications/importDependency.swift
@@ -1,1 +1,1 @@
-        .Package(url: "https://github.com/jkingoliver/bms-pushnotifications-serversdk-swift.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/ibm-bluemix-mobile-services/bms-pushnotifications-serversdk-swift.git", majorVersion: 0, minor: 5),
