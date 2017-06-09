99da27ff9a4bfe784d406d4bbc0ac9cc85f84f0b
Taylor Franklin <tfranklin@us.ibm.com>
2017-06-09 12:34:22 -0500

    updated sdk gen url to production

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {};
 
-config.sdkGenURL = 'https://mobilesdkgen.stage1.ng.bluemix.net/sdkgen/api/generator/';
+config.sdkGenURL = 'https://mobilesdkgen.ng.bluemix.net/sdkgen/api/generator/';
 
 module.exports = config;
