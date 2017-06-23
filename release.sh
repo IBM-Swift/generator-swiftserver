22e3a12a3cdcce290d0d5c2897d852413604fd4c
Robert Deans <robert.deans@uk.ibm.com>
2017-06-23 16:23:12 +0100

    Updated to use the bot credentials

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 # Script to release to NPM and publish tag
 if [[ $TRAVIS_BRANCH == "master" ]] && [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
   git remote rm origin
-  git remote add origin https://<BOT_NAME>:<BOT_TOKEN>@github.com/IBM-Swift/generator-swiftserver
+  git remote add origin https://SwiftDevOps:${GH_TOKEN}@github.com/IBM-Swift/generator-swiftserver
   VERSION=`node -e "console.log(require('./package.json').version);"`
   echo "Creating release ${VERSION}" 
   git tag $VERSION && git push origin $VERSION || true
@@ -12,7 +12,7 @@ if [[ $TRAVIS_BRANCH == "master" ]] && [[ $TRAVIS_PULL_REQUEST == "false" ]]; th
   # Deleting the old release branch
   BRANCH_TO_DELETE=updateTo$VERSION
   echo Deleting $BRANCH_TO_DELETE 
-  curl -u <BOT_NAME>:<BOT_TOKEN> -X DELETE -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/IBM-Swift/generator-swiftserver/branches/$BRANCH_TO_DELETE/protection"
+  curl -u SwiftDevOps:${GH_TOKEN} -X DELETE -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/IBM-Swift/generator-swiftserver/branches/$BRANCH_TO_DELETE/protection"
   git push origin :$BRANCH_TO_DELETE
 else
   echo "This not on master or a push to master - exiting"
--- review.sh
+++ review.sh
@@ -8,19 +8,18 @@ then
   then 
     echo This is a merge from master - exiting
   else
-    git config user.email "travis@travis-ci.org"
+    USER=SwiftDevOps:${GH_TOKEN}
     git config user.name "Generator bot"
     git config push.default simple
     git remote rm origin
-    # TODO: Create bot account and insert details
-    #git remote add origin https://<BOT_NAME>:<BOT_TOKEN>@github.com/IBM-Swift/generator-swiftserver
+    git remote add origin https://${USER}@github.com/IBM-Swift/generator-swiftserver
     echo Creating branch with the new version to be reviewed
     git checkout -b temp
     echo Updating the version
     standard-version -i "CHANGES.md" --tag-version
 
     # Delete all the other branches that are release branches
-    PULLS=$(curl -u <BOT_NAME>:<BOT_TOKEN> -X GET -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/IBM-Swift/generator-swiftserver/pulls")
+    PULLS=$(curl -u ${USER} -X GET -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/IBM-Swift/generator-swiftserver/pulls")
     LENGTH=$(echo $PULLS | jq 'length')
     SIZE=$(( $LENGTH - 1 ))
     # Loop through all the different pull requests and delete the release branches
@@ -31,7 +30,7 @@ then
       if [[ $BASE == "master" ]] && [[ $PULL_BRANCH =~ $BRANCH_REGEX ]]; then
         # Unprotect the branch before deleting
         echo Removing protection on $PULL_BRANCH
-        curl -u <BOT_NAME>:<BOT_TOKEN> -X DELETE -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/IBM-Swift/generator-swiftserver/branches/$PULL_BRANCH/protection"
+        curl -u ${USER} -X DELETE -H "Accept: application/vnd.github.loki-preview+json" "https://api.github.com/repos/IBM-Swift/generator-swiftserver/branches/$PULL_BRANCH/protection"
         echo Deleting $PULL_BRANCH
         git push origin :$PULL_BRANCH
       fi
@@ -42,10 +41,10 @@ then
     git branch -m $BRANCH
     git push -u origin $BRANCH
     # Protect the branch from force push and deletion
-    curl -u <BOT_NAME>:<BOT_TOKEN>rob-deans:${GH_TOKEN} -X PUT -H "Content-Type: application/json" -H "Accept: application/vnd.github.loki-preview+json" -d "{\"required_status_checks\": {\"strict\": true,\"contexts\": []},\"required_pull_request_reviews\":{},\"restrictions\": null,\"enforce_admins\": true}" "https://api.github.com/repos/IBM-Swift/generator-swiftserver/branches/$BRANCH/protection"
+    curl -u ${USER} -X PUT -H "Content-Type: application/json" -H "Accept: application/vnd.github.loki-preview+json" -d "{\"required_status_checks\": {\"strict\": true,\"contexts\": []},\"required_pull_request_reviews\":{},\"restrictions\": null,\"enforce_admins\": true}" "https://api.github.com/repos/IBM-Swift/generator-swiftserver/branches/$BRANCH/protection"
 
     # create a pull request to master
-    curl -u <BOT_NAME>:<BOT_TOKEN> -H "Content-Type: application/json" -X POST -d "{\"title\": \"Release ${VERSION}\", \"head\": \"${BRANCH}\", \"base\": \"master\", \"body\": \"Do NOT attempt to push further changes to this branch.\"}" https://api.github.com/repos/IBM-Swift/generator-swiftserver/pulls 
+    curl -u ${USER} -H "Content-Type: application/json" -X POST -d "{\"title\": \"Release ${VERSION}\", \"head\": \"${BRANCH}\", \"base\": \"master\", \"body\": \"Do NOT attempt to push further changes to this branch.\"}" https://api.github.com/repos/IBM-Swift/generator-swiftserver/pulls 
 
   fi
 else
