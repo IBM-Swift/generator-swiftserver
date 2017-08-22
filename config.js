d14ec460a4cfa81fd68424601b815b76c36a69f4
Neil Hardman <nhardman@uk.ibm.com>
2017-08-22 11:01:57 +0100

    set timeout values

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {}
 
 config.sdkGenURL = 'https://mobilesdkgen.ng.bluemix.net/sdkgen/api/generator/'
-config.sdkGenCheckDelay = 3000
+config.sdkGenCheckDelay = 6000
 
 module.exports = config
