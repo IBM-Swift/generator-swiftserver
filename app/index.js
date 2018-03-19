9e8686237dc86e5315e030443380707105fd30c8
Christian Compton <cbcompto@us.ibm.com>
2018-03-19 12:01:46 -0500

    fix: provide healthcheck routes by default

--- app/index.js
+++ app/index.js
@@ -134,9 +134,7 @@ module.exports = Generator.extend({
         var docker = isTrue(this.options.docker) || undefined
         var usecase = isTrue(this.options.enableUsecase) || undefined
         var starterOptions = this.options.starterOptions || undefined
-        console.log(this.options.healthcheck)
         var healthcheck = (typeof this.options.healthcheck === 'undefined') ? true : isTrue(this.options.healthcheck)
-        console.log(healthcheck)
 
         var web = (this.appType === 'web' || this.appType === 'bff' || undefined)
         var hostSwagger = (this.appType === 'bff' || undefined)
